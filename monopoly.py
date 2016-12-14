3f7e20e9580ffc9024b94691be148efe27b65a15
rwbogl <robert.w.bliss@gmail.com>
2016-12-14 10:35:58 -0500

    Enable reporting of space names and numbers

--- monopoly.py
+++ monopoly.py
@@ -353,7 +353,10 @@ def report_steady(chain):
                             reverse=True)
 
     # Grab the string of the space name.
-    sorted_strings = [str(pair[0]) for pair in sorted_pairs]
+    # We treated "jail" special, so just pass it through.
+    sorted_strings = [standard_monopoly_map[pair[0]] + " ({})".format(pair[0])
+                        if pair[0] != "jail" else "Jail (10)"
+                            for pair in sorted_pairs]
     # Align them so that they print pretty.
     sorted_strings = space_align(sorted_strings)
 
@@ -363,6 +366,48 @@ def report_steady(chain):
     for index, aligned_space in enumerate(sorted_strings):
         prob = sorted_pairs[index][-1]
         print("    {}:".format(aligned_space), prob)
+
+standard_monopoly_map = { 0: "GO"
+                        , 1: "Mediterranean Avenue"
+                        , 2: "Community Chest 1"
+                        , 3: "Baltic Avenue"
+                        , 4: "Income Tax"
+                        , 5: "Reading Railroad"
+                        , 6: "Oriental Avenue"
+                        , 7: "Chance 1"
+                        , 8: "Vermont Avenue"
+                        , 9: "Connecticut Avenue"
+                        , 10: "Jail"
+                        , 11: "St. Charles Place"
+                        , 12: "Electric Company"
+                        , 13: "States Avenue"
+                        , 14: "Virginia Avenue"
+                        , 15: "Pennsylvania Railroad"
+                        , 16: "St. James Place"
+                        , 17: "Community Chest"
+                        , 18: "Tennessee Avenue"
+                        , 19: "New York Avenue"
+                        , 20: "Free Parking"
+                        , 21: "Kentucky Avenue"
+                        , 22: "Chance 2"
+                        , 23: "Indiana Avenue"
+                        , 24: "Illinois Avenue"
+                        , 25: "B. & O. Railroad"
+                        , 26: "Atlantic Avenue"
+                        , 27: "Ventnor Avenue"
+                        , 28: "Water Works"
+                        , 29: "Marvin Gardens"
+                        , 30: "Go To Jail"
+                        , 31: "Pacific Avenue"
+                        , 32: "North Carolina Avenue"
+                        , 33: "Community Chest"
+                        , 34: "Pennsylvania Avenue"
+                        , 35: "Short Line"
+                        , 36: "Chance 3"
+                        , 37: "Park Place"
+                        , 38: "Luxury Tax"
+                        , 39: "Boardwalk"
+                    }
 
 if __name__ == "__main__":
     monopoly_chain = make_pykov_monopoly(40, 2, 10, 30)
